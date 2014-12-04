9e8bb698695b134190d9e8119c7957b7b658023b
Chris N <cninden@gmail.com>
2014-12-04 00:23:40 -0800

    sync

--- MthToLastElement/code.py
+++ MthToLastElement/code.py
@@ -4,11 +4,13 @@ import sys
 inFileName = sys.argv[1]
 
 with open(inFileName) as inFile:
-	lines = inFile.readlines()
-    for l in lines:
-    	line = l.strip()
-    	parts = line.split(' ')
-    	num = int(parts[-1:])
-    	parts = parts[0:(len(parts)-1)]
-    	print parts[num%len(parts)]
-	
+  lines = inFile.readlines()
+  for l in lines:
+    line = l.strip()
+    parts = line.split(' ')
+    num = int(parts[-1:][0])
+    if num <= len(parts)-1:
+      parts = parts[0:(len(parts)-1)]
+      print parts[num%len(parts)]
+    else:
+      parts[0]
